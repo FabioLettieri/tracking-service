3462a54dd0c0c1af0aafeecdbf7a80f5c6ea94c4
BLett <fabiolettieri09@gmail.com>
2025-01-27 20:43:08 -0300

    EntityBase correction and V001 flyway update

--- src/main/java/com/flsolution/mercadolivre/tracking_service/entities/EntityBase.java
+++ src/main/java/com/flsolution/mercadolivre/tracking_service/entities/EntityBase.java
@@ -3,6 +3,9 @@ package com.flsolution.mercadolivre.tracking_service.entities;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -16,6 +19,8 @@ public abstract class EntityBase implements Serializable {
 
 	private static final long serialVersionUID = -6332486979595010787L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private LocalDateTime createdAt;
--- src/main/resources/db/migration/V001__create_table_packs.sql
+++ src/main/resources/db/migration/V001__create_table_packs.sql
@@ -2,11 +2,11 @@ CREATE TABLE packs (
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     sender VARCHAR(255) NOT NULL,
-    recipient VARCHAR(255)
-    isHolliday BOOLEAN,
+    recipient VARCHAR(255),
+    is_holliday BOOLEAN,
     fun_fact VARCHAR(255),
     estimated_delivery_date VARCHAR(255),
-    status VARCHAR(50) DEFAULT 'CREATED',
+    status VARCHAR(50),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     is_active BOOLEAN DEFAULT TRUE
